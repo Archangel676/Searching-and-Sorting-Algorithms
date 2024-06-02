5bf74bc743b5b929ca64d190b08f9849e5524e59
Maya <mvenega9@my.chemeketa.edu>
2024-06-02 22:01:33 +0000

    Jun 2, 2024, 3:01 PM

--- helpers.cpp
+++ helpers.cpp
@@ -1,3 +1,7 @@
 #include "helpers.h"
 
 // Any needed implementations should be in helpers.cpp.
+
+// When using a vector as a parameter should be a reference parameter:
+// vector<Region>& regions
+// Use .at(i)
