6e6aa868c1965df5e940f3844a8853b1e03c1072
Maya <mvenega9@my.chemeketa.edu>
2024-06-02 21:59:56 +0000

    Jun 2, 2024, 2:59 PM

--- main.cpp
+++ main.cpp
@@ -3,10 +3,14 @@
 using namespace std;
 
 // One main function that does each of the tasks below in order.
+
 // You should use functions to make your main a manageable length and easy to
-// understand. Anything you are asked to print out should be printed in your
-// final program with a clear label. That is your proof that your code is
-// working.
+// understand.
+
+// Anything you are asked to print out should be printed in your final program
+// with a clear label.
+
+// That is your proof that your code is working.
 
 int main()
 {
