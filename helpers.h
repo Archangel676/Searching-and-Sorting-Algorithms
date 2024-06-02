7172c08f54ab684b1be1e0623934b97afde83476
Maya <mvenega9@my.chemeketa.edu>
2024-06-02 22:10:10 +0000

    Jun 2, 2024, 3:10 PM

--- helpers.h
+++ helpers.h
@@ -1,6 +1,16 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
-// Any structs/classes/functions you define should be in helpers.h
+// using std::string
+//  Any structs/classes/functions you define should be in helpers.h
+
+struct Region {
+  int ID;
+  string cityName;
+  string state;
+};
+// Each Region consists of an , a e, and a .
+
+// Create a class or struct to store Regions.
 
 #endif HELPERS_H
