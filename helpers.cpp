38a053103023fdeae8f17735789ddc7abb6ce61c
Maya <mvenega9@my.chemeketa.edu>
2024-06-02 21:58:08 +0000

    Jun 2, 2024, 2:58 PM

--- helpers.cpp
+++ helpers.cpp
@@ -1,1 +1,3 @@
 #include "helpers.h"
+
+// Any needed implementations should be in helpers.cpp.
--- helpers.h
+++ helpers.h
@@ -1,4 +1,6 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
+// Any structs/classes/functions you define should be in helpers.h
+
 #endif HELPERS_H
