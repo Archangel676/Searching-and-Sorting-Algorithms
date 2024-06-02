e3df241af2d7b30834c3ded343f52f5eb998dc59
Maya <mvenega9@my.chemeketa.edu>
2024-06-02 22:34:47 +0000

    Jun 2, 2024, 3:34 PM

--- helpers.cpp
+++ helpers.cpp
@@ -17,12 +17,25 @@ string getInput(ifstream& addressFile)
     return addressString;
 }
 
-string getAreaCode(const string& input)
+string getRegion(const string& input)
 {
   size_t commaOne = input.find(",");
   size_t commaTwo = input.find(",", commaOne + 1);
 
   // 20330,San Francisco,CA
+  // start 1 = 0
+  // comma 1 = 5
+  // end 1 = 4
+  // length = comma 1
+
+  // start 2 = 6
+  // comma 2 = 19
+  // end 2 = 18
+  // length = comma 2 - start 2
+
+  // start 3 = 20
+  // end 3 = end
+
   // 1st param = Position of the first character to be copied as a substring.
   // 2nd param = Number of characters to include in the substring,
   // default: all characters until the end of the string.
@@ -30,11 +43,11 @@ string getAreaCode(const string& input)
   // size_t areaCodeLength = fullAreaCode.length();
   // size_t cityLength = fullAreaCode.length();
 
-  string areaCode = input.substr(commaTwo + 1);
-  string city = input.substr(commaTwo + 1);
+  string areaCode = input.substr(0, commaOne - 1);
+  string city = input.substr(commaOne + 1, ());
   string state = input.substr(commaTwo + 1);
 
-  return "formatedAreaCode";
+  return areaCode + city + state;
 }
 
 // Write code to open the file and then read in each line, parse the data, and
