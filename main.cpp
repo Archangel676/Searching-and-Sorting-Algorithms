4648c6e48869b9a890e5105ba6780432f0880acd
Maya <mvenega9@my.chemeketa.edu>
2024-06-02 21:59:09 +0000

    Jun 2, 2024, 2:59 PM

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+// One main function that does each of the tasks below in order.
+// You should use functions to make your main a manageable length and easy to
+// understand. Anything you are asked to print out should be printed in your
+// final program with a clear label. That is your proof that your code is
+// working.
+
 int main()
 {
   cout << "-----------------------------Part 1-----------------------------"
