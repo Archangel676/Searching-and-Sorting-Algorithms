344648a53c2a86c17ce0d34b091ea830de2440f9
Maya <mvenega9@my.chemeketa.edu>
2024-06-03 00:50:35 +0000

    Jun 2, 2024, 5:50 PM

--- helpers.h
+++ helpers.h
@@ -9,23 +9,23 @@ using namespace std;
 //   Any structs/classes/functions you define should be in helpers.h
 
 struct Region {
-  int ID;
-  string cityName;
-  string state;
+  int ID = 0;
+  string cityName = "";
+  string state = "";
+
+  /**
+   * @brief Get a single full address line from the input.txt file
+   * @return string Full address as a string
+   */
+  string getInput(ifstream& addressFile);
+
+  /**
+   * @brief Takes the full address and returns the area code of the address as a
+   * string
+   * @param input Full address from input
+   * @return string Returns a string of just the area code in digits
+   */
+  string getRegion(const string& input);
 };
-
-/**
- * @brief Get a single full address line from the input.txt file
- * @return string Full address as a string
- */
-string getInput(ifstream& addressFile);
-
-/**
- * @brief Takes the full address and returns the area code of the address as a
- * string
- * @param input Full address from input
- * @return string Returns a string of just the area code in digits
- */
-string getRegion(const string& input);
 
 #endif // HELPERS_H
--- main.cpp
+++ main.cpp
@@ -17,7 +17,8 @@ int main()
   cout << "-----------------------------Part 1-----------------------------"
        << endl;
   // TO Do: Part 1
-  string s = getRegion("20330,San Francisco,CA");
+  Region r;
+  string s = r.getRegion("20330,San Francisco,CA");
   cout << s;
 
   // cout << "-----------------------------Part 2-----------------------------"
